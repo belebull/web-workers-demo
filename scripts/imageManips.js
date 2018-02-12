326d5e2b757f9112e2647cebf43c23f7e27bcd9d
beleiciabullock <beleiciabullock@gmail.com>
2018-02-12 16:32:11 -0500

    added worker and edited manipulation function for better performance

--- scripts/imageManips.js
+++ scripts/imageManips.js
@@ -1,8 +1,8 @@
- // Image manipulation logic from github.com/jwill/psychic-lana
+// Image manipulation logic from github.com/jwill/psychic-lana
 
-function manipulate(type, r, g, b, a) {
-  
-  var func = function() {};
+function getManipFunc(type) {
+
+  var func = function () { };
 
   function makePixelInverted(r, g, b, a) {
     r = 255 - r;
@@ -59,5 +59,5 @@ function manipulate(type, r, g, b, a) {
       break;
   }
 
-  return func(r, g, b, a);
+  return func;
 }
